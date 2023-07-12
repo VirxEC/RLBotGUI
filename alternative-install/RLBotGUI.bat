0475c1cda4f5add39c0511b7bc422a328ee608f9
VirxEC <virx@virxcase.dev>
2023-07-12 09:32:18 -0400

    New alt installer

--- alternative-install/RLBotGUI.bat
+++ alternative-install/RLBotGUI.bat
@@ -5,51 +5,41 @@ echo Installing RLBotGUI if necessary, then launching!
 if not exist "%LocalAppData%\RLBotGUIX" mkdir "%LocalAppData%\RLBotGUIX"
 pushd "%LocalAppData%\RLBotGUIX"
 
-if not exist "%LocalAppData%\RLBotGUIX\Python37" (
+set rl_py="Python37\python.exe"
+set rl_pip="Python37\Scripts\pip.exe"
+
+if not exist %rl_py% (
   echo Looks like we're missing RLBot's Python ^(3.7.9^), installing...
 
   powershell -command "Expand-Archive '%~dp0python-3.7.9-custom-amd64.zip' '%LocalAppData%\RLBotGUIX\Python37'"
-
-  if exist "%LocalAppData%\RLBotGUIX\venv\pyvenv.cfg" (
-    echo Old venv detected, updating Python location so we don't have to reinstall...
-    rem This is a custom python script that updates the Python location in the venv
-    "%LocalAppData%\RLBotGUIX\Python37\python.exe" "%~dp0\update_venv.py"
-  )
 )
-
-rem Create a virtual environment which will isolate our package installations from any
-rem existing python installation that the user may have.
-
-if not exist .\venv\Scripts\activate.bat (
-  echo Creating Python virtual environment just for RLBot...
-  "%LocalAppData%\RLBotGUIX\Python37\python.exe" -m venv .\venv
-  if %ERRORLEVEL% GTR 0 (
-    echo Something went wrong with creating Python virtual environment, aborting.
-    pause
-    exit
-  )
-)
-
-rem Activate the virtual environment
-call .\venv\Scripts\activate.bat
 
 rem We ping google.com to see if we have an internet connection
 rem We then store the output of the command to nul which prevents the command from printing to the console
 ping -n 1 google.com > nul
 if %errorlevel% == 0 (
+  if not exist %rl_pip% (
+    echo Python pip not found - installing!
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+  )
+
   echo Installing / upgrading RLBot components...
-  python -m pip install --upgrade pip
-  pip install wheel
-  pip install eel
-  pip install --upgrade rlbot_gui rlbot
+
+  %rl_py% -m pip install -U pip --no-warn-script-location
+  %rl_pip% install setuptools wheel --no-warn-script-location
+  %rl_pip% install eel --no-warn-script-location
+  %rl_pip% install -U rlbot_gui rlbot --no-warn-script-location
 ) else (
+  echo.
   echo It looks like you're offline, skipping package upgrades.
   echo Please note that if this is your first time running RLBotGUI, an internet connection is required to properly install.
+  echo.
 )
 
 echo Launching RLBotGUI...
 
-python -c "from rlbot_gui import gui; gui.start()"
+%rl_py% -c "from rlbot_gui import gui; gui.start()"
 
 if %ERRORLEVEL% GTR 0 (
   pause
